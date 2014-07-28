2681c5d65e9cf89621958a0a13da7415a9386000
Kevin Nguyen <nguyenk1@umbc.edu>
2014-07-27 21:52:48 -0400

    July27

--- parseLC.py
+++ parseLC.py
@@ -1,4 +1,4 @@
-
+import operator
 import argparse
 import sys
 import os
@@ -22,9 +22,10 @@ def main():
           
           rowList = line.split()
           pairing = rowList[0]+"+"+rowList[1]
+          otherPair = rowList[1]+"+"+rowList[0]
           min = rowList[3]+"."+rowList[2]+"."+"min"
           max = rowList[4]+"."+rowList[2]+"."+"max"
-          if pairing not in linkedContig:
+          if pairing not in linkedContig or otherPair not in linkedContig:
                linkedContig[pairing] = set([(min,max)])
           else:
                linkedContig[pairing].add((min,max))
@@ -51,12 +52,12 @@ def main():
           for i in range(len(combineFirst[thing])):
 
                agreeCover = []
-               
                c = i
-               while  c <= len(combineFirst[thing])-1:
-                    if  c == len(combineFirst[thing]) -1 or combineFirst[thing][c].split(".")[-1] == "max":
+               while c <= len(combineFirst[thing])-1:
+                    if c == len(combineFirst[thing]) -1 or combineFirst[thing][c].split(".")[-1] == "max":
                          
                          agreeCover = tuple(agreeCover)
+                         
                          if agreeCover != ():
                               groupCover[thing].append(agreeCover)
                          
@@ -64,25 +65,59 @@ def main():
                          c =c+ 1
                     elif combineFirst[thing][c].split(".")[-1] == "min":
                          agreeCover.append(combineFirst[thing][c])
+                         
                          c=c+ 1
 
 
-          groupCover[thing] = set(groupCover[thing])
-     newFile = open('combineFirst', 'w')
-     for stuff in combineFirst:
-          newFile.write(stuff)  
-          newFile.write("\n")
-          newFile.write(" ".join(combineFirst[stuff]))
-          newFile.write("\n")
-          
 
+          groupCover[thing] = set(groupCover[thing])
+     final = []
+     for stuff in groupCover:
           gclist = list(groupCover[stuff])
      
           for item in gclist:
-               newList = []
+               gapList = []
+               fragList = []
                for x in item:
-                    #item is a tuple
                     index = sortedSE[stuff][0].index(x) 
-                    newList.append((sortedSE[stuff][0][index].split(".")[0],sortedSE[stuff][1][index].split(".")[0])), 
-          print stuff.split("+")[0] + "\t" + stuff.split("+")[1] + "\t " + newList[-1][0] + "\t" + newList[0][-1] + "\t" + str(len(newList)) + "\t" + str(newList)
+                    gapList.append((sortedSE[stuff][0][index].split(".")[0],sortedSE[stuff][1][index].split(".")[0])), 
+                    fragList.append(sortedSE[stuff][0][index].split(".")[1])
+               final.append([stuff.split("+")[0], stuff.split("+")[1], gapList[-1][0], gapList[0][-1], str(len(gapList)),(sorted(fragList, key = lambda x: int(x[5:]))), (gapList)])
+     final = sorted(final, key = lambda x: int(x[0]))
+     final = sorted(final, key = lambda x: int(x[4]), reverse = True)
+     lastPrint = []
+
+     for l in range(len(final)):
+          if l == 0:
+               lastPrint.append(final[l])
+          else:
+               listL = set(final[l][5])
+               listOri = set(lastPrint[-1][5]) 
+#               print listL
+ #              print listOri
+#               print type(listL)
+ #              print type(listOri)
+         #print  listOri.issuperset(listL) 
+               if listOri.issuperset(listL) == False:
+                    lastPrint.append(final[l])
+
+     lastPrint= sorted(lastPrint, key = lambda z: int(z[0]))
+     forRealLast = []
+
+     for u in range(len(lastPrint)):
+
+          if u == 0:
+               forRealLast.append(lastPrint[u])
+          else:
+               listU = lastPrint[u][5]
+
+               listPrev = forRealLast[-1][5]
+
+               if set(listPrev).issuperset(set(listU)) == False:
+                    forRealLast.append(lastPrint[u])
+
+     forRealLast = sorted(forRealLast, key = lambda t: int(t[0]))
+     for doit in forRealLast:
+          print '\t'.join(str(w) for w in doit)
+          
 main()
--- testing.py
+++ testing.py
@@ -20,7 +20,15 @@ le")
 
      #extracts the gMin and gMax
      G = nx.Graph()
-
+     G1 = nx.Graph()
+     G2 = nx.Graph() 
+     G3 = nx.Graph()
+     G4 = nx.Graph()
+     G5 = nx.Graph()
+     G6 = nx.Graph()
+     G7 = nx.Graph()
+     G8 = nx.Graph()
+     graphList = [G1, G2, G3, G4, G5, G6,G7,G8]
      lessFive = []
      lessTen = []
      less20 = []
@@ -33,46 +41,100 @@ le")
 
      for line in file:
           rowList = line.split("\t")
-          numSup = rowList[4]
+          numSup = int(rowList[4])
           contig1 = int(rowList[0])
           contig2 = int(rowList[1])
      
-          if numSup < 5:
-               G.add_edge(rowList[0],rowList[1])
+          if numSup <= 5:
+               G1.add_edge(rowList[0],rowList[1])
                lessFive.append(G.add_edge(rowList[0],rowList[1]))
-          if numSup >= 5 and rowList[4] < 10:
-               G.add_edge(rowList[0],rowList[1])
+          
+          elif numSup > 5 and numSup <= 10:
+               G2.add_edge(rowList[0],rowList[1])
                lessTen.append(G.add_edge(rowList[0],rowList[1]))
-          if numSup >= 10 and rowList[4] < 20:
-               G.add_edge(rowList[0],rowList[1])
+               
+          elif numSup > 10 and numSup <= 20:
+               G3.add_edge(rowList[0],rowList[1])
                less20.append(G.add_edge(rowList[0],rowList[1]))      
-          if numSup >= 20 and rowList[4] < 30:
-               G.add_edge(rowList[0],rowList[1])
+          elif numSup > 20 and numSup <= 30:
+               G4.add_edge(rowList[0],rowList[1])
                less30.append(G.add_edge(rowList[0],rowList[1]))
-          if numSup >= 30 and rowList[4] < 40:
-               G.add_edge(rowList[0],rowList[1])
+          elif numSup > 30 and numSup <= 40:
+               
+               G5.add_edge(rowList[0],rowList[1])
                less40.append(G.add_edge(rowList[0],rowList[1]))
-          if numSup >= 40 and rowList[4] < 50:
-               G.add_edge(rowList[0],rowList[1])
+          elif numSup > 40 and numSup <= 50:
+               G6.add_edge(rowList[0],rowList[1])
                less50.append(G.add_edge(rowList[0],rowList[1]))
-          if numSup >= 50 and rowList[4] < 60:
-               G.add_edge(rowList[0],rowList[1])
+          elif numSup > 50 and numSup <= 60:
+               G7.add_edge(rowList[0],rowList[1])
                less60.append(G.add_edge(rowList[0],rowList[1]))
-          if numSup >= 60:
-               G.add_edge(rowList[0],rowList[1])
+          elif numSup > 60:
+               G8.add_edge(rowList[0],rowList[1])
                great60.append(G.add_edge(rowList[0],rowList[1]))
-     all =  [ lessFive, lessTen, less20, less30, less40 ,less50, less60, great60]
+     all = [lessFive, lessTen, less20, less30, less40 ,less50, less60, great60]
 
-      
+     print 
+     total = 0
+     for graphs in all:
+
+          total = total + len(graphs)
+
      pos = nx.spring_layout(G)
-     nx.draw_networkx_nodes(G,pos, node_size = 20)
      i = 0
-     colorList = ['w','y','c','g','b','M','r','K']
+     colorList = ['w','y','c','g','b','m','r','k']
      for list in all:
-                    
+          
+         # pos = nx.spring_layout(graphList[i])
+          
+          #nx.draw_networkx_nodes(graphList[i],pos, node_size = 20)          
           nx.draw_networkx_edges(G, pos, edge_color = colorList[i])
           i = i + 1
      
-     plt.savefig("graph.png", dpi=1000)
+     plt.savefig("graphTotal.png")
+     plt.clf()
+     print len(G1.edges())
+     print G1.edges()
+     print len(G2.edges())
+     print G2.edges()
+     print len(G3.edges())
+     print G3.edges()
+     print len(G4.edges())
+     print G4.edges()
+     print len(G5.edges())
+     print G5.edges()
+     print len(G6.edges())
+     print G6.edges()
+     print len(G7.edges())
+     print G7.edges()
+     print len(G8.edges())
+     print G8.edges()
+     print len(G.edges())
+     print G.edges()
+
+     nx.draw(G1, pos = nx.spring_layout(G1), nos_size = 20)
+     plt.savefig("graph1.png")
+     plt.clf()
+     nx.draw(G2, pos = nx.spring_layout(G2))
+     plt.savefig("graph2.png")
+     plt.clf()
+     nx.draw(G3, pos = nx.spring_layout(G3))
+     plt.savefig("graph3.png")
+     plt.clf()
+     nx.draw(G4, pos = nx.spring_layout(G4))
+     plt.savefig("graph4.png")
+     plt.clf()
+     nx.draw(G5, pos = nx.spring_layout(G5))
+     plt.savefig("graph5.png")
+     plt.clf()
+     nx.draw(G6, pos = nx.spring_layout(G6))
+     plt.savefig("graph6.png")
+     plt.clf()
+     nx.draw(G7, pos = nx.spring_layout(G7))
+     plt.savefig("graph7.png")
+     plt.clf()
+     nx.draw(G8, pos = nx.spring_layout(G8))
+     plt.savefig("graph8.png")
+
 main()
                     
