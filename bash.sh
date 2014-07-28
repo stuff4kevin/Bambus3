715b686b1228d39084b125893dc0a95e1cb3c6b3
Kevin Nguyen <nguyenk1@umbc.edu>
2014-07-27 22:29:14 -0400

    July27

--- bash.sh
+++ bash.sh
@@ -1,6 +1,4 @@
-python parseSAM.py lib1.sam 3 linkedContigs
+python parseSAM.py lib1.sam linkedContigs
 sort -k1 linkedContigs.txt > temp
 python parseLC.py temp > temp2
-sort -rk5 temp2 | awk '$5 > 1' | awk '$3 > 0' | awk '$4 > 0' > final.txt
-python testing.py final.txt
-display graph.pdf
+sort -rnk5 temp2 | awk '$5 > 1' | awk '$3 > 0' | awk '$4 > 0' > final.txt
